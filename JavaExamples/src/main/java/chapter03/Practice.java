1354a5327e771bd81d828bd85f3cefd5e52560f3
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-10-08 12:52:37 -0500

    Few more practice question

--- JavaExamples/src/main/java/chapter03/Practice.java
+++ JavaExamples/src/main/java/chapter03/Practice.java
@@ -1,26 +1,75 @@
 package chapter03;
 
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
+
 public class Practice {
 
-        public static void main(String[] args) {
-            int x = 10;
-            if (x > 5) {
-                int y = 20;
-                System.out.println(x + y);
-            }
-            System.out.println(x);
+    static public void main(String[] args) {
+//            int x = 10;
+//            if (x > 5) {
+//                int y = 20;
+//                System.out.println(x + y);
+//            }
+//            System.out.println(x);
+//
+//            String original = "hello";
+//            String reversed = "";
+//
+//            // Loop through the string starting from the last character
+//            for (int i = original.length() - 1; i >= 0; i--) {
+//                reversed += original.charAt(i);  // Append each character in reverse order
+//            }
+//
+//            // Output the reversed string
+//            System.out.println("Reversed string: " + reversed);
 
-            String original = "hello";
-            String reversed = "";
+        //------------------------------------------------------------------------------------------------
 
-            // Loop through the string starting from the last character
-            for (int i = original.length() - 1; i >= 0; i--) {
-                reversed += original.charAt(i);  // Append each character in reverse order
-            }
+//            int[] arr = {10, 324, 45, 90, 9808};
 
-            // Output the reversed string
-            System.out.println("Reversed string: " + reversed);
-        }
+//            int max = arr[0];
+//
+//
+//            for (int i = 1; i < arr.length;i++){
+//                if (arr[i] > max) {
+//                    max = arr[i];
+//                }
+//            }
+//                System.out.println("Largest in given array is " + max);
 
 
+        //-------------------------------------------------------------------------------------
+
+//        String original = "hello";
+//        String reversed = "";
+//
+//        for (int i = original.length() - 1; i >= 0; i--) {
+//            reversed += original.charAt(i);
+//        }
+//
+//        System.out.println(reversed);
+//
+//    }
+
+    //-----------------------------------------------------------------------------------
+
+//    int[] arr = {2, 10, 324, 45, 90, 9808};
+//
+//    int min = arr[0];
+//
+//    for(int i = 1;i<arr.length;i++)
+//
+//    {
+//        if (arr[i] < min) {
+//            min = arr[i];
+//        }
+//
+//     }
+//        System.out.println("smallest in given array is " + min);
+
+        int result = 10 / 0;
+        System.out.println( result );
+}
 }
